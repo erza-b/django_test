884b209322b0abba818715acc1a98f745c1850d0
Erza Berbatovci <116487812+erza-b@users.noreply.github.com>
2023-12-30 10:48:52 +0100

    added delete-post

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PagesListView,PagesDetailView,PagesCreateView,PagesUpdateView
+from .views import PagesListView,PagesDetailView,PagesCreateView,PagesUpdateView,PagesDeleteView
 
 urlpatterns = [
+    path('post/<int:pk>/delete/',PagesDeleteView.as_view(),name='post_delete'),
     path('post/<int:pk>/edit/', PagesUpdateView.as_view(), name='post_edit'),
     path('post/new/',PagesCreateView.as_view(),name='post_new'),
     path('',PagesListView.as_view(), name='home'),
